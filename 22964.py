5ee9648390803909808c62ab7f0292e7b969d1e4
송인범 <72247209+InbumS@users.noreply.github.com>
2022-01-31 16:05:32 +0900

    그리디

---
+++ 22964.py
@@ -0,0 +1,21 @@
+a,b,c,m=map(int,input().split())
+
+ans=0
+fatigue=0
+time=1
+
+for i in range(24):
+    if fatigue+a<=m:
+        fatigue+=a
+        ans+=b     
+        
+    else:
+        fatigue-=c
+        if fatigue<0:
+            fatigue=0
+        
+        
+if a>m:
+    print(0)
+else:
+    print(ans)
