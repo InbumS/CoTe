2f75abed56712ef70ca8d9f3e13d5117c7b59790
송인범 <72247209+InbumS@users.noreply.github.com>
2022-12-31 00:58:39 +0900

    2022/12/30

---
+++ KWStudy/k번째 약수.py
@@ -0,0 +1,17 @@
+
+from os import kill
+import sys 
+sys.stdin=open("input.txt", "rt")
+
+n,k=map(int, input().split())
+cnt=0
+
+for i in range(1,n+1):
+    if n%i==0:
+        cnt+=1
+   
+    if cnt==k:
+        print(i)
+        break
+else:
+    print(-1)
