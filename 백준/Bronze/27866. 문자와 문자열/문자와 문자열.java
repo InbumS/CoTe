ccc2ecb8f3b9c49b07af3de822f5296f1815d582
송인범 <72247209+InbumS@users.noreply.github.com>
2024-01-25 00:17:11 +0900

    [Bronze V] Title: 문자와 문자열, Time: 208 ms, Memory: 17760 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.java
@@ -0,0 +1,19 @@
+
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String word =sc.nextLine();
+        int n = sc.nextInt();
+        System.out.println(word.charAt(n-1));
+
+        }
+    }
+
+
+
+
+
