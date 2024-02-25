c1fec75fb6fb82a9ce75f180d6faf417a24831ae
송인범 <72247209+InbumS@users.noreply.github.com>
2024-02-25 19:14:22 +0900

    [Silver III] Title: N과 M (8), Time: 604 ms, Memory: 22732 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int n;
+	static int m;
+	static boolean visited[];
+	static int[] record;
+	static int[] arr;
+	public static void main(String[] args)throws IOException {
+		BufferedReader  br = new BufferedReader(new InputStreamReader(System.in));
+	    StringTokenizer st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m =Integer.parseInt(st.nextToken());
+		arr = new int [n];
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<n; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(arr);
+		record = new int [m];
+		johab(0, 0);
+	}
+	
+	static void johab(int start, int idx) {
+		if(idx == m) {
+			for(int i=0; i<m; i++)
+			{
+				System.out.print(record[i]+" ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		int compare =-1;
+		for(int i=start; i<n; i++) {
+			int what = arr[i];
+			if(what == compare)continue;
+			record[idx] = arr[i];
+			compare = what;
+			johab(i, idx+1);
+		}
+		
+	}
+	
+		
+}
